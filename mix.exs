11f72f43f579b472959a0abea95a41775316b9aa
Luca Ongaro <mail@lucaongaro.eu>
2020-04-13 19:24:49 +0200

    Add info in mix.esx

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule CubQ.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: "https://github.com/lucaong/cubq",
+      docs: [
+        main: "CubQ"
+      ]
     ]
   end
 
@@ -21,6 +26,16 @@ defmodule CubQ.MixProject do
     [
       {:cubdb, "~> 0.17 or ~> 1.0"},
       {:dialyxir, "~> 1.0.0", only: [:dev], runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      description: "An embedded queue and stack abstraction for Elixir on top of CubDB",
+      files: ["lib", "LICENSE", "mix.exs"],
+      maintainers: ["Luca Ongaro"],
+      licenses: ["Apache-2.0"],
+      links: %{}
     ]
   end
 end
